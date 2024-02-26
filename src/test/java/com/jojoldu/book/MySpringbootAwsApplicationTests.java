318d01420dfca381e509a4e50648e5e8a757c985
hj.jeon <zunom@naver.com>
2024-02-26 10:24:28 +0900

    chaper 01. 인텔리제이로 스프링부트 시작하기

---
+++ src/test/java/com/jojoldu/book/MySpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
